2a16054d45a330f095a90346ce24a5412efb66f7
Dilip Valiya <dilipkvaliya@gmail.com>
2024-08-28 19:23:47 +0530

    add env file, change type to module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import "dotenv/config";
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
