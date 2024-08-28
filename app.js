ff12aa5941b2742500fc2729cbda2ff10004cb5c
Dilip Valiya <dilipkvaliya@gmail.com>
2024-08-28 17:50:00 +0530

    feat: create a blog app

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const posts = [
+  {
+    title: "Project Description",
+    content:
+      "The goal of this project is to create a Blog web application using Node.js, Express.js, and EJS. The application will allow users to create and view blog posts. Posts will not persist between sessions as no database will be used in this version of the application.",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("home", { posts: posts });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postContent,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle = req.params.postName.toLowerCase();
+
+  posts.forEach((post) => {
+    const storedTitle = post.title.toLowerCase();
+    if (storedTitle === requestedTitle) {
+      res.render("post", { title: post.title, content: post.content });
+    }
+  });
+
+  res.render("post", { title: "404", content: "No Post found!" });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
